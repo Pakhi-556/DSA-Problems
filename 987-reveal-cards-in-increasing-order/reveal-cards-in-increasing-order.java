1e6a155aec65be391852fd77cce2594d1f8dfae7
Pakhi Gupta <127219703+Pakhi-556@users.noreply.github.com>
2024-04-10 11:53:49 +0530

    Time: 4 ms (86.41%) | Memory: 43.5 MB (52.91%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n=deck.length;
+        Arrays.sort(deck);
+        Deque<Integer> dq=new ArrayDeque();
+
+        dq.addFirst(deck[n-1]);
+        for(int i=n-2;i>=0;i--){
+            dq.addFirst(dq.removeLast());
+            dq.addFirst(deck[i]);
+        }
+
+        for(int i=0;i<n;i++){
+            deck[i]=(int)dq.removeFirst();
+        }
+
+        return deck;
+        
+    }
+}
