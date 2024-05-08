56e577b6042c450eef735c5b4331220f7bf78767
Pakhi Gupta <127219703+Pakhi-556@users.noreply.github.com>
2024-05-08 10:37:37 +0530

    Time: 9 ms (65.84%) | Memory: 45.6 MB (42.85%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,7 +1,7 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
 
-      int N = score.length;
+     /* int N = score.length;
 
         // Create a max heap of pairs (score, index)
         PriorityQueue<Pair<Integer, Integer>> heap = new PriorityQueue<>(
@@ -27,6 +27,39 @@ class Solution {
             }
             place++;
         }
-        return rank;
+        return rank;*/
+
+        int n=score.length;
+        //create priority queue that store hightest peak value
+        PriorityQueue<Pair<Integer, Integer>> q = new PriorityQueue<>(
+                (a, b) -> b.getKey() - a.getKey());
+        String ans[]=new String[n];
+        
+        for(int i=0;i<score.length;i++){
+            q.add(new Pair(score[i],i));
+        }
+        int count=1;
+        while(!q.isEmpty()){
+             Pair<Integer,Integer> curr=q.poll();
+             int ori=curr.getValue();
+
+             if(count==1){
+                ans[ori]="Gold Medal";
+             }
+
+            else if(count==2){
+                ans[ori]="Silver Medal";
+            }
+            else if(count==3){
+                 ans[ori]="Bronze Medal";
+            }
+            else{
+                ans[ori]=Integer.toString(count);
+            }
+
+            count++;
+        }
+
+       return ans;
     }
 }
