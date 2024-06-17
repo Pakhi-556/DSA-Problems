29b2571276d2c11983683a6cdeecf5c2f2c0e90c
Pakhi Gupta <127219703+Pakhi-556@users.noreply.github.com>
2024-06-17 17:24:11 +0530

    Time: 5 ms (20.27%) | Memory: 40.3 MB (35.29%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+
+        long i=0;
+        long j=(long)Math.sqrt(c);
+
+        while(i<=j){
+            if(i*i+j*j==c){
+                return true;
+            }
+
+            else if(i*i+j*j > c){
+                j--;
+               
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+        
+    }
+}
