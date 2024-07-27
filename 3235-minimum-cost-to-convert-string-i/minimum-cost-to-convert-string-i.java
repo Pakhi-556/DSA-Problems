4f7e08c6bceaa5ba1fc2adea06d089115e82d243
Pakhi Gupta <127219703+Pakhi-556@users.noreply.github.com>
2024-07-27 11:07:03 +0530

    Time: 12 ms (98.17%) | Memory: 45.9 MB (27.98%) - LeetSync

---
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -0,0 +1,36 @@
+class Solution {
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        // using floyad warshall algoritm in graph to short path betweeen to node from each of its node
+        int[][] dis = new int[26][26];
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(dis[i], Integer.MAX_VALUE);
+            dis[i][i] = 0;
+        }
+        for (int i = 0; i < cost.length; i++) {
+            dis[original[i] - 'a'][changed[i] - 'a'] = Math.min(dis[original[i] - 'a'][changed[i] - 'a'], cost[i]);
+        }
+        for (int k = 0; k < 26; k++) {
+            for (int i = 0; i < 26; i++)
+                if (dis[i][k] < Integer.MAX_VALUE) {
+                    for (int j = 0; j < 26; j++) {
+                        if (dis[k][j] < Integer.MAX_VALUE) {
+                            dis[i][j] = Math.min(dis[i][j], dis[i][k] + dis[k][j]);
+                        }
+                    }
+                }
+        }
+        long count=0L;
+         for (int i = 0; i < source.length(); i++) {
+            int c1 = source.charAt(i) - 'a';
+            int c2 = target.charAt(i) - 'a';
+            if (dis[c1][c2] == Integer.MAX_VALUE) {
+                return -1L;
+            } else {
+                count+= (long)dis[c1][c2];
+            }
+        }
+        return count;
+
+
+    }
+}
