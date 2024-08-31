766b0840b8db13d4f8f7ba93de447c3c60cf3322
Pakhi Gupta <127219703+Pakhi-556@users.noreply.github.com>
2024-08-31 16:35:22 +0530

    Time: 10 ms (89.39%) | Memory: 57.9 MB (54.53%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums.length==1){
+            return false;
+        }
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        
+        return false;
+    }
+}
