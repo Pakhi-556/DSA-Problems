ba83ecf35adcadc803b2b6c94f78790506e933af
Pakhi Gupta <127219703+Pakhi-556@users.noreply.github.com>
2024-09-07 07:55:14 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (18.69%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        return dfs(head,head,root);
+    }
+
+    public boolean dfs(ListNode head,ListNode curr, TreeNode root){
+        if(curr==null){
+            return true;
+        }
+
+        if(root==null){
+            return false;
+        }
+
+        if(curr.val==root.val){
+            curr=curr.next;
+        }
+
+        else if(head.val==root.val){
+            head=head.next;
+        }
+        else{
+            curr=head;
+        }
+
+        return dfs(head,curr,root.left)||dfs(head,curr,root.right);
+    }
+}
